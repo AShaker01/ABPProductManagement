8f32878de49c40714565eb75e00a3c089ee9110e
AShaker01 <ahmed.shaker1707@gmail.com>
2024-02-15 23:02:47 +0200

    Add project files.

---
+++ src/ProductManagement.Application/Products/ProductAppService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Domain.Repositories;
+
+namespace ProductManagement.Products
+{
+    public class ProductAppService(IRepository<Product, Guid> productRepo) : ProductManagementAppService, IProductAppService
+    {
+        private readonly IRepository<Product, Guid> _productRepository = productRepo;
+        public async Task<PagedResultDto<ProductDto>> GetListAsync(PagedAndSortedResultRequestDto input)
+        {
+            var queryable = await _productRepository.WithDetailsAsync(x => x.Category);
+            queryable = queryable
+            .Skip(input.SkipCount)
+            .Take(input.MaxResultCount)
+            .OrderBy(input.Sorting ?? nameof(Product.Name));
+
+            var products = await AsyncExecuter.ToListAsync(queryable);
+            var count = await _productRepository.GetCountAsync();
+            return new PagedResultDto<ProductDto>(count, ObjectMapper.Map<List<Product>, List<ProductDto>>(products));
+        }
+    }
+}
---
+++ test/ProductManagement.EntityFrameworkCore.Tests/EntityFrameworkCore/Applications/Products/ProductAppServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using ProductManagement.Products;
+using ProductManagement.Samples;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Xunit;
+
+namespace ProductManagement.EntityFrameworkCore.Applications.Products
+{
+    [Collection(ProductManagementTestConsts.CollectionDefinitionName)]
+    public class ProductAppServiceTests : SampleAppServiceTests<ProductManagementEntityFrameworkCoreTestModule>
+    {
+        private readonly IProductAppService _productAppService;
+        public ProductAppServiceTests()
+        {
+            _productAppService = GetRequiredService<IProductAppService>();
+        }
+        [Fact]
+        public async Task Should_Get_Product_List()
+        {
+            //Act
+            var output = await _productAppService.GetListAsync(new PagedAndSortedResultRequestDto());
+            //Assert
+            output.TotalCount.ShouldBe(3);
+            output.Items.ShouldContain(x => x.Name.Contains("Acme Monochrome Laser Printer"));
+        }
+    }
+}
